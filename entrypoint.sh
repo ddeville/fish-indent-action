e7e4c30f5b38329890ccf0c988de11a3cc5d58f7
Damien Deville <damien@ddeville.me>
2023-01-10 23:24:01 -0800

    Implement entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 
-set -e
+# First get the path to the files that have errors
+errors=$( { find . -name "*.fish" -print0 | xargs -0 fish_indent --check; }  2>&1 )
 
-echo "name=HELLO WORLD" >> $GITHUB_OUTPUT
+# Second, for each file that has errors, print a diff to the output
+count=0
+while IFS= read -r path; do
+  if [ -n "$path" ]; then
+    df=$(fish_indent "$path" | diff -u "$path" -)
+    echo "error=$df" >>"$GITHUB_OUTPUT"
+    ((count++))
+  fi
+done <<< "$errors"
 
-# find . -name "*.fish" -print0 | xargs -0 fish_indent --check
-# fish_indent config/common/.config/fish/config.fish | diff -u config/common/.config/fish/config.fish -
+# Third, return an error if any file had errors
+if [ "$count" -gt 0 ]; then
+  echo "fish_indent failed"
+  exit 1
+fi
