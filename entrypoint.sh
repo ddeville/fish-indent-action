2bb02d7fb71c4155e705ec42eb15a6fc8bd0a83d
Damien Deville <damien@ddeville.me>
2023-01-10 23:33:37 -0800

    fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,7 @@ count=0
 while IFS= read -r path; do
   if [ -n "$path" ]; then
     df=$(fish_indent "$path" | diff -u "$path" -)
-    # echo "error=$df" >>"$GITHUB_OUTPUT"
-    echo "::set-output name=output::$df"
+    echo "output=$df" >>"$GITHUB_OUTPUT"
     ((count++))
   fi
 done <<< "$errors"
