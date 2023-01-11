3d3caee6688471833d7f3d26e9fa1f06afa9d1ee
Damien Deville <damien@ddeville.me>
2023-01-10 22:50:17 -0800

    derp

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "HELLO WORLD" >> $GITHUB_OUTPUT
+echo "name=HELLO WORLD" >> $GITHUB_OUTPUT
 
 # find . -name "*.fish" -print0 | xargs -0 fish_indent --check
 # fish_indent config/common/.config/fish/config.fish | diff -u config/common/.config/fish/config.fish -
